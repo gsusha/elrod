410d4b4911ba4c8a3bd0c8f5b884539f96bea567
gsusha <x.dyachenko@gmail.com>
2021-06-28 22:12:05 +0300

    shop test

---
+++ js/loadItems.js
@@ -0,0 +1,43 @@
+$('document').ready(function(){
+	loadItems();
+});
+
+function loadItems() {
+	$.getJSON('/json/hoverboards.json', function(data){
+		var hover = "";
+		for (var key in data){
+			item += '<div class="market__item">';
+			item += '<div class="market__name">' + data[key]['name'] + '</div>';
+			item += '<div class="market__img"><img src="' + data[key]['img'] + '"></div>';
+			item += '<div class="market__price">' + data[key]['price'] + '</div>';
+			item += '<div class="market__desc"><ul>';
+			item += '<li>' + data[key]['desc1'] + ': <span>' + data[key]['value1'] + '</span></li>';
+			item += '<li>' + data[key]['desc2'] + ': <span>' + data[key]['value2'] + '</span></li>';
+			item += '<li>' + data[key]['desc3'] + ': <span>' + data[key]['value3'] + '</span></li>';
+			item += '<li>' + data[key]['desc4'] + ': <span>' + data[key]['value4'] + '</span></li>';
+			item += '<li>' + data[key]['desc5'] + ': <span>' + data[key]['value5'] + '</span></li>';
+			item += '</div></ul>';
+			item += '<div class="market__btn"><button class="order-btn" onClick="openModal();">Заказать</button></div>';
+			item += '</div>'
+		}
+		$('#hoverboardItems').html(item);
+	});
+
+	$.getJSON('/json/e-scooters.json', function(data){
+		var item = "";
+		for (var key in data){
+			item += '<div class="market__item">';
+			item += '<div class="market__name">' + data[key]['name'] + '</div>';
+			item += '<div class="market__img"><img src="' + data[key]['img'] + '"></div>';
+			item += '<div class="market__price">' + data[key]['price'] + '</div>';
+			item += '<div class="market__desc"><ul>';
+			item += '<li>' + data[key]['desc1'] + ': <span>' + data[key]['value1'] + '</span></li>';
+			item += '<li>' + data[key]['desc2'] + ': <span>' + data[key]['value2'] + '</span></li>';
+			item += '<li>' + data[key]['desc3'] + ': <span>' + data[key]['value3'] + '</span></li>';
+			item += '</div></ul>';
+			item += '<div class="market__btn"><button class="order-btn" onClick="openModal();">Заказать</button></div>';
+			item += '</div>'
+		}
+		$('#eScootersItems').html(item);
+	})
+}
