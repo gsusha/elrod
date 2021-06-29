5299b02b5fd4db13a8ad76d29a502e64f180fad3
gsusha <x.dyachenko@gmail.com>
2021-06-29 18:54:42 +0300

    burger fix

--- js/form.js
+++ js/form.js
@@ -1,25 +1,19 @@
-// Получить модальный
 var modal = document.getElementById("modal");
 
-// Получить кнопку, которая открывает модальный
 var btn = document.getElementById("order-btn");
 
-// Получить элемент <span>, который закрывает модальный
 var span = document.getElementsByClassName("modal__close")[0];
 
-// Когда пользователь нажимает на кнопку, откройте модальный
 function openModal() {
   modal.style.display = "block";
   document.body.style.overflow = "hidden";
 }
 
-// Когда пользователь нажимает на <span> (x), закройте модальное окно
 span.onclick = function() {
   modal.style.display = "none";
   document.body.style.overflow = "";
 }
 
-// Когда пользователь щелкает в любом месте за пределами модального, закройте его
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
