9e9f848ef38af10b11d2fa09d7fb915362518147
gsusha <x.dyachenko@gmail.com>
2021-07-01 19:33:21 +0300

    btn fix

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,3 @@
-/* 
-function filterHide(producer) {
-	marketItemArr = document.getElementsByClassName(producer);
-	for (var i = 0; i < marketItemArr.length; i++) {
-		item = marketItemArr[i];
-		if (item.style.display == "flex") {
-			item.style.display = "none";
-		} else {
-			item.style.display = "flex";
-		}
-	}
-}
-*/
-
 $(document).ready(function() {
 	$('#kugoo').click(function(event) {
 		$('#kugoo').toggleClass('active');
